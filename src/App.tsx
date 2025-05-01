267974dc7df59a88eac84d7fb7a45f3992f738f4
Aynaz <jkz1713x2@gmail.com>
2025-05-01 16:50:57 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>dwadadawd</div>
+      <div>fefef</div>
+      <div>fefefef</div>
+    </>
+  );
+}
+
+export default App;
